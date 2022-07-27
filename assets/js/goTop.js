74cf83995448eb599b5059ff57bac8dade6cfbd5
Besart Ibishi <bi47125@ubt-uni.net>
2022-07-27 12:23:52 +0200

    Added go on top button

---
+++ assets/js/goTop.js
@@ -0,0 +1,18 @@
+var goTopButton = document.getElementsByClassName("goTopBtn")[0];
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    goTopButton.style.display = "block";
+  } else {
+    goTopButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function goToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
